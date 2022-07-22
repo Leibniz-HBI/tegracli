b94217be2463f53463f805d639ca1973b267737d
Philipp Kessling <p.kessling@leibniz-hbi.de>
2022-07-23 00:12:29 +0200

    [update] test_dispatcher module docstring

--- tests/test_dispatchers.py
+++ tests/test_dispatchers.py
@@ -1,6 +1,9 @@
 """ # Dispatcher Functions Test
 
 This tests suit concerns itself with the asynchronous dispatcher functions.
+
+These tests should be marked both api and socket enabled, since we actually want data from Telegram.
+This has the implication that we need valid credentials in a valid `tegracli.conf.yml`. 
 """
 
 from pathlib import Path
@@ -42,7 +45,12 @@ def test_search(queries: list[str], client: TelegramClient):
 @pytest.mark.parametrize("params", [{"limit": 10}, {"limit": 2, "reverse": True}])
 @pytest.mark.parametrize("queries", [["channelnotfound123"], ["channel", "1004347112"]])
 def test_get(queries: list[str], client: TelegramClient, params: Dict):
-
+    """ 
+    Asserts
+    -------
+    
+    Should not throw exception
+    """
     # client = Mock(TelegramClient)
     # client.loop = Mock(AbstractEventLoop)
     with client:
